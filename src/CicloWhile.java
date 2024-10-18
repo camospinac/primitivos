0df4fab28ea5d85459eafac4318b64277f4184ef
Camilo Ospina <camospinac@outlook.com>
2024-10-17 23:06:28 -0500

    Ciclo While

---
+++ src/CicloWhile.java
@@ -0,0 +1,10 @@
+public class CicloWhile {
+    public static void main(String[] args) throws Exception{
+        
+        var contador = 0;
+        while (contador < 5) {
+            System.out.println("Contador: " + contador);
+            contador++;
+        }
+    }
+}
