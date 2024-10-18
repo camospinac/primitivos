472f607d252ada00d175d4a86d9f0d53ad004546
Camilo Ospina <camospinac@outlook.com>
2024-10-17 23:12:35 -0500

    ajuste do while

--- src/CicloWhile.java
+++ src/CicloWhile.java
@@ -3,8 +3,14 @@ public class CicloWhile {
         
         var contador = 0;
         while (contador < 5) {
-            System.out.println("Contador: " + contador);
+            System.out.println("\nContador: " + contador);
             contador++;
         }
+
+        var contador2 = 0;
+        do {
+            System.out.println("\nContador: " + contador2);
+            contador2 += 1;
+        } while (contador2 < 10);
     }
 }
