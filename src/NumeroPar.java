99b38fa2bc38886aa6ba1bae02a0eed0d9eb49f9
Camilo Ospina <camospinac@outlook.com>
2024-10-18 22:52:15 -0500

    Ternarios y Pares

--- src/NumeroPar.java
+++ src/NumeroPar.java
@@ -1,3 +1,13 @@
+// Número par o impar: Determina si un número entero es par o impar usando el operador módulo.
+
+import java.util.Scanner;
 public class NumeroPar {
-    p
+    public static void main(String[] args) throws Exception{
+        int num;
+        var scanner = new Scanner(System.in);
+        System.out.println("Por favor digite el numero: ");
+        num = Integer.parseInt(scanner.nextLine());
+        var par =  (num%2 == 0) ? "El numero es par" : "El numero es impar";
+        System.out.println(par);
+    }
 }
