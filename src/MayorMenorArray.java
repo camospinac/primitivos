32bf44c92ff15883e88c0335c7ff66d33a1f6d8b
Camilo Ospina <camospinac@outlook.com>
2024-10-23 22:38:52 -0500

    Ejercicio Arrays Menor Mayor

---
+++ src/MayorMenorArray.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class MayorMenorArray {
+    public static void main(String[] args) throws Exception{
+        var scanner = new Scanner(System.in);
+        int largo;
+        System.out.print("Por favor digite el largo del array: ");
+        largo = Integer.parseInt(scanner.nextLine());
+        int [] enteros = new int[largo];
+
+        for(int i = 0; i < largo; i++){
+
+        }
+
+    }
+
+}
