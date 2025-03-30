c14a0ea5d8ed53a5f7012b284f8ecafae3b4f90a
Camilo Ospina <camospinac@outlook.com>
2025-03-30 15:46:52 -0500

    ajustes codigo ejercicios, inicio matrices

---
+++ src/matrices.java
@@ -0,0 +1,16 @@
+public class matrices {
+    public static void main(String[] args) {
+        // Definicion matriz 2x3
+        int [][] matriz = new int[2][2];
+        //Insertar valores a la matriz
+        matriz[0][0]= 200;
+        matriz[0][1]= 300;
+        matriz[1][0]= 400;
+        matriz[1][1]= 600;
+        System.out.println("Valores de la matriz [0][0]: " + matriz[0][0]);
+        System.out.println("Valores de la matriz [0][1]: " + matriz[0][1]);
+        System.out.println("Valores de la matriz [1][0]: " + matriz[1][0]);
+        System.out.println("Valores de la matriz [1][1]: " + matriz[1][1]);
+    }
+    
+}
